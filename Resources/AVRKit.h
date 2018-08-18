e23e54132a3b13ec5994c29317f1cf95dad67c03
Ramon Kleiss <ramonkleiss@gmail.com>
2018-08-18 17:00:27 +0200

    Initial commit

---
+++ Resources/AVRKit.h
@@ -0,0 +1,7 @@
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AVRKit.
+FOUNDATION_EXPORT double AVRKitVersionNumber;
+
+//! Project version string for AVRKit.
+FOUNDATION_EXPORT const unsigned char AVRKitVersionString[];
